57671405b9cb96580ef918d35e815002b4a30052
svt13 <svt13@ukr.net>
2023-01-09 18:39:39 +0200

    Add project files.

---
+++ PhoneDB/Model/PhoneModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneDB.Model
+{
+    public class PhoneModel
+    {
+        public int Id { get; set; }
+        public string Producer { get; set; } = null!;
+        public string Model { get; set; } = null!;
+        public decimal Price { get; set; }
+
+    }
+}
